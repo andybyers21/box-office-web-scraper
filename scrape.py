a41f13b224ab639068467466843eddd8699f130a
andybyers21 <andy.byers21@gmail.com>
2020-10-08 13:57:44 +0100

    clean code into functions

--- scrape.py
+++ scrape.py
@@ -1,7 +1,12 @@
 import datetime
+import os
 import pandas as pd
 import requests
 from requests_html import HTML
+
+
+BASE_DIR = os.path.dirname(__file__)
+
 
 now = datetime.datetime.now()
 year = now.year
@@ -20,41 +25,41 @@ def url_to_txt(url, filename="world.html", save=False):
 
 url = 'https://www.boxofficemojo.com/year/world/'
 
-html_text = url_to_txt(url)
 
-r_html = HTML(html=html_text)
-table_class = ".imdb-scroll-table"
-# table_class = "#table"
-r_table = r_html.find(table_class)
+def parse_and_extract(url, name='2020'):
+    html_text = url_to_txt(url)
+    r_html = HTML(html=html_text)
+    table_class = ".imdb-scroll-table"
+    # table_class = "#table"
+    r_table = r_html.find(table_class)
+    table_data = []
+    header_names = []
+    # print(r_table)
 
-table_data = []
-header_names = []
+    # Verify that r_table is a list, parse data.
+    if len(r_table) == 1:
+        parsed_table = r_table[0]           # table
+        rows = parsed_table.find("tr")      # rows
+        header_row = rows[0]                # assume first row is header
+        header_cols = header_row.find("th")
+        header_names = [x.text for x in header_cols]
 
-# print(r_table)
-
-# Verify that r_table is a list, parse data.
-if len(r_table) == 1:
-    parsed_table = r_table[0]           # table
-    rows = parsed_table.find("tr")      # rows
-    header_row = rows[0]                # assume first row is header
-    header_cols = header_row.find("th")
-
-    header_names = [x.text for x in header_cols]
-
-    for row in rows[1:]:
-        # loop rows and cols, return raw data at specific point
-        # print(row.text)
-        cols = row.find("td")
-        row_data = []
-        for i, col in enumerate(cols):
-            # print(i, col.text, '\n\n')
-            row_data.append(col.text)
-        table_data.append(row_data)
-
-
-# print(header_names)
-# print(table_data)
+        for row in rows[1:]:
+            # loop rows and cols, return raw data at specific point
+            # print(row.text)
+            cols = row.find("td")
+            row_data = []
+            for i, col in enumerate(cols):
+                # print(i, col.text, '\n\n')
+                row_data.append(col.text)
+            table_data.append(row_data)
+        df = pd.DataFrame(table_data, columns=header_names)
+        path = os.path.join(BASE_DIR, 'data')
+        os.makedirs(path, exist_ok=True)
+        filepath = os.path.join(
+            f'data/boxofficemojo {name} rankings.csv')
+        df.to_csv(filepath, index=False)
 
 
-df = pd.DataFrame(table_data, columns=header_names)
-df.to_csv('boxofficemojo.csv', index=False)
+url = 'https://www.boxofficemojo.com/year/world/2020'
+parse_and_extract(url)
