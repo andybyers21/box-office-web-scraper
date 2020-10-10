d166e614c58194248d8aa15203bd0a8578187e78
andybyers21 <andy.byers21@gmail.com>
2020-10-10 11:46:40 +0100

    return error if year does not exsist

--- scrape.py
+++ scrape.py
@@ -17,7 +17,7 @@ def url_to_txt(url, filename="world.html", save=False):
             with open(f"world-{year}.html", "w") as f:
                 f.write(html_text)
         return html_text
-    return ""
+    return None
 
 
 url = 'https://www.boxofficemojo.com/year/world/'
@@ -25,6 +25,8 @@ url = 'https://www.boxofficemojo.com/year/world/'
 
 def parse_and_extract(url, name='2020'):
     html_text = url_to_txt(url)
+    if html_text == None:
+        return False
     r_html = HTML(html=html_text)
     table_class = ".imdb-scroll-table"
     # table_class = "#table"
@@ -34,28 +36,30 @@ def parse_and_extract(url, name='2020'):
     # print(r_table)
 
     # Verify that r_table is a list, parse data.
-    if len(r_table) == 1:
-        parsed_table = r_table[0]           # table
-        rows = parsed_table.find("tr")      # rows
-        header_row = rows[0]                # assume first row is header
-        header_cols = header_row.find("th")
-        header_names = [x.text for x in header_cols]
+    if len(r_table) == 0:
+        return False
+    parsed_table = r_table[0]           # table
+    rows = parsed_table.find("tr")      # rows
+    header_row = rows[0]                # assume first row is header
+    header_cols = header_row.find("th")
+    header_names = [x.text for x in header_cols]
 
-        for row in rows[1:]:
-            # loop rows and cols, return raw data at specific point
-            # print(row.text)
-            cols = row.find("td")
-            row_data = []
-            for i, col in enumerate(cols):
-                # print(i, col.text, '\n\n')
-                row_data.append(col.text)
-            table_data.append(row_data)
-        df = pd.DataFrame(table_data, columns=header_names)
-        path = os.path.join(BASE_DIR, 'data')
-        os.makedirs(path, exist_ok=True)
-        filepath = os.path.join(
-            f'data/boxofficemojo {name} rankings.csv')
-        df.to_csv(filepath, index=False)
+    for row in rows[1:]:
+        # loop rows and cols, return raw data at specific point
+        # print(row.text)
+        cols = row.find("td")
+        row_data = []
+        for i, col in enumerate(cols):
+            # print(i, col.text, '\n\n')
+            row_data.append(col.text)
+        table_data.append(row_data)
+    df = pd.DataFrame(table_data, columns=header_names)
+    path = os.path.join(BASE_DIR, 'data')
+    os.makedirs(path, exist_ok=True)
+    filepath = os.path.join(
+        f'data/boxofficemojo {name} rankings.csv')
+    df.to_csv(filepath, index=False)
+    return True
 
 
 def run(start_year=None, years_ago=1):
@@ -67,8 +71,11 @@ def run(start_year=None, years_ago=1):
     assert len(f"{start_year}") == 4
     for i in range(0, years_ago + 1):
         url = f"https://www.boxofficemojo.com/year/world/{start_year}/"
-        parse_and_extract(url, name=start_year)
-        print(f"finished {start_year}")
+        finished = parse_and_extract(url, name=start_year)
+        if finished:
+            print(f"finished {start_year}")
+        else:
+            print(f"{start_year} does data not exist")
         start_year -= 1
 
 
