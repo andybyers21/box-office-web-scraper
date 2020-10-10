4b1c530fc951653a8f0846af3e5a5c5d0537c7fb
andybyers21 <andy.byers21@gmail.com>
2020-10-10 19:21:39 +0100

    clean and document code

--- scrape.py
+++ scrape.py
@@ -8,30 +8,34 @@ from requests_html import HTML
 
 BASE_DIR = os.path.dirname(__file__)
 
+url = 'https://www.boxofficemojo.com/year/world/'
 
-def url_to_txt(url, filename="world.html", save=False):
-    """ check if status code 200 get data for defined year from boxofficemojo.com with python requests """
+
+def url_to_txt(url):
+    """ Get data from boxofficemojo.com using requests.
+
+    Check for status code 200.
+    Retrieve data for defined year.
+    """
     r = requests.get(url)
     if r.status_code == 200:
         html_text = r.text
-        if save:
-            with open(f"world-{year}.html", "w") as f:
-                f.write(html_text)
         return html_text
     return None
 
 
-url = 'https://www.boxofficemojo.com/year/world/'
-
-
 def parse_and_extract(url, name='2020'):
-    """ loop through rows & cols to Verify that r_table is a list.
-        Parse HTML & CSS.
-        Return raw data as CSV.
+    """ Parse data from url and extract relevant info.
+
+    Loop through rows & cols to verify that r_table is a list.
+    Parse HTML & CSS.
+    Return raw data as CSV.
     """
     html_text = url_to_txt(url)
+
     if html_text == None:
         return False
+
     r_html = HTML(html=html_text)
     table_class = ".imdb-scroll-table"
     r_table = r_html.find(table_class)
@@ -40,8 +44,9 @@ def parse_and_extract(url, name='2020'):
 
     if len(r_table) == 0:
         return False
-    parsed_table = r_table[0]           # table
-    rows = parsed_table.find("tr")      # rows
+
+    parsed_table = r_table[0]
+    rows = parsed_table.find("tr")
     header_row = rows[0]                # assume first row is header
     header_cols = header_row.find("th")
     header_names = [x.text for x in header_cols]
@@ -52,23 +57,29 @@ def parse_and_extract(url, name='2020'):
         for i, col in enumerate(cols):
             row_data.append(col.text)
         table_data.append(row_data)
+
     df = pd.DataFrame(table_data, columns=header_names)
     path = os.path.join(BASE_DIR, 'data')
     os.makedirs(path, exist_ok=True)
-    filepath = os.path.join(
-        f'data/boxofficemojo {name} rankings.csv')
+    filepath = os.path.join(f'data/boxofficemojo {name} rankings.csv')
     df.to_csv(filepath, index=False)
     return True
 
 
 def run(start_year=None, years_ago=1):
-    """ Run the scraper, scrape multiple iterations of... """
+    """ Run the scraper.
+
+    Assert start year & number of years ago
+    scrape multiple iterations of... 
+    """
     if start_year == None:
         now = datetime.datetime.now()
         start_year = now.year
+
     assert isinstance(start_year, int)
     assert isinstance(years_ago, int)
     assert len(f"{start_year}") == 4
+
     for i in range(0, years_ago + 1):
         url = f"https://www.boxofficemojo.com/year/world/{start_year}/"
         finished = parse_and_extract(url, name=start_year)
